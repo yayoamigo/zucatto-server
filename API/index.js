31654612c59f7b17f3f7f79482324057a356b957
david zambrano corral <Yayoamigo>
2023-03-21 18:43:46 -0500

    Add jwt authentication

--- API/index.js
+++ API/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
 
 dotenv.config();
 
@@ -13,7 +16,9 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
 const app = express();
 
 app.use(express.json());
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen( 5000, () => {
     console.log("Backend server is running!");
   });
